b25811cd8f363e43bd3072b89549994824e0a791
Ben Mizelle <Ben Mizelle>
2017-04-25 21:22:06 -0400

    initial setup and heroku deployment

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Require mysql
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "newcastle22",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+
+});
+
+// Export connection
+module.exports = connection;
